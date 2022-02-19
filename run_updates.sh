69ad324e9cbafb2fc404f31eedfd15799c3544d0
Chris Rodgers <xrodgers@gmail.com>
2022-02-19 17:46:09 -0500

    doc

--- run_updates.sh
+++ run_updates.sh
@@ -1,2 +1,3 @@
 # Run without sudo and type password when prompted
+# probably need to replace pull with git merge upstream/player-12.5.9
 git pull && make clean && make && sudo make install && sudo /etc/init.d/vmware restart
